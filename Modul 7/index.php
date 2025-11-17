3636fc86f506c047ab212df03cf9f6c52f112fd8
fatintsani <fatin.tsani231@gmail.com>
2025-11-17 17:26:58 +0700

    update modul 7

---
+++ Modul 7/index.php
@@ -0,0 +1,9 @@
+<?php
+// echo "Hello, World!";
+
+$nama = "Fatin";
+$jurusan = "D3 Teknologi Komputer";
+
+echo "Nama: " . $nama . "<br>";
+echo "Jurusan: " . $jurusan . "<br>";
+echo "Belajar PHP itu menyenangkan!";
