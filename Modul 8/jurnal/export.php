a4447ec6a2d97d2218d98cbecfac217cc39dd6df
Fatin Muflihuts Tsani <145127234+fatintsani@users.noreply.github.com>
2025-12-04 16:42:31 +0700

    Add files via upload

---
+++ Modul 8/jurnal/export.php
@@ -0,0 +1,17 @@
+<?php
+$data = json_decode(file_get_contents("data.json"), true);
+
+header("Content-Type: text/csv");
+header("Content-Disposition: attachment; filename=buku_tamu.csv");
+
+$fp = fopen("php://output", "w");
+
+fputcsv($fp, ["Nama", "Alamat", "No HP", "Keperluan", "Waktu"]);
+
+foreach ($data as $row) {
+    fputcsv($fp, $row);
+}
+
+fclose($fp);
+exit;
+?>
